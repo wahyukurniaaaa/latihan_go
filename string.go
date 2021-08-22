d776c8e3a603ab3df8ca5cacaa8b6fe90ed07a05
Wahyu Kurnia Prambudi <wahyu.kurnia@phiraka.com>
2021-08-23 04:58:27 +0700

    initial commit

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("wahyu")
+	fmt.Println("wahyu kurnia")
+	fmt.Println("wahyu kurnia prambudi")
+	fmt.Println(len("bayu"))
+	fmt.Println("wahyu kurnia"[0])
+	fmt.Println("wahyu kurnia prambudi"[1])
+}
---
+++ variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var name string
+
+	name = "wahyu kurnia prambudi"
+	fmt.Println(name)
+
+	name = "wahyu kurnia"
+	fmt.Println(name)
+
+	var surname = "papam"
+	fmt.Println(surname)
+
+	country := "indonesia"
+	fmt.Println(country)
+
+	var(
+		firstName = "Wahyu"
+		middleName = "Kurnia"
+		LastName = "Prambudi"
+	)
+
+	fmt.Println("nama saya adalah ",firstName,middleName,LastName)
+
+}
